feba30306772c7a6a25eb7c1b27e107cf4bf61ec
SapnaVishwakrma <vsapna903@gmail.com>
2024-07-26 20:53:10 +0530

    This is doctor page

--- fronted/src/app/page.tsx
+++ fronted/src/app/page.tsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import Home from '@/components/Home'
 import LoginForm from '@/app/login/page.js'
+import DoctorPage from '@/components/Doctorpage/doctorpage';
 
 
 const page = () => {
   return (
     <div>
-       <Home /> 
-       
+
+       <Home/>
 
     </div>
   )
---
+++ fronted/src/components/Doctorpage/doctorpage.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+export default function DoctorPage() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <header className="h-16 "></header> 
+      <main className="max-w-4xl mx-auto py-8 px-4 md:px-6 lg:px-8 xl:px-12">
+        <div className="bg-white p-6 md:p-8 lg:p-12 rounded-lg shadow-lg mb-12">
+          <div className="flex flex-col md:flex-row justify-between items-start">
+            <div className="text-black space-y-4">
+              <p>Specialist:</p>
+              <p>Time:</p>
+              <p className="flex-1">Address:</p> 
+              <p>Fees:</p>
+            </div>
+            <div className="flex flex-col items-center mt-4 md:mt-0">
+              <div className="w-24 h-24 bg-gray-300 rounded-full mb-2"></div>
+              <div className="text-teal-500">Dr. XYZ</div>
+            </div>
+          </div>
+        </div>
+
+        <section className="mb-16">
+          <h2 className="text-xl font-bold mb-4 text-black">About Doctor</h2> 
+        </section>
+
+        <section className="mb-16">
+          <h2 className="text-xl font-bold mb-4 text-black">Work Experience</h2> 
+        </section>
+
+        <section className="mb-16">
+          <h2 className="text-xl font-bold mb-4 text-black">Awards</h2> 
+        </section>
+
+        <section className="mb-12">
+          <div className="flex flex-col sm:flex-row justify-between space-y-4 sm:space-y-0 sm:space-x-4">
+            <div className="bg-white p-4 rounded-lg shadow-lg text-black flex-1 h-36">Review 1</div>
+            <div className="bg-white p-4 rounded-lg shadow-lg text-black flex-1 h-36">Review 2</div>
+            <div className="bg-white p-4 rounded-lg shadow-lg text-black flex-1 h-36">Review 3</div>
+          </div>
+        </section>
+
+        <div className="text-center mt-8">
+          <button className="bg-teal-500 text-white py-2 px-4 rounded">Book Appointment</button>
+        </div>
+      </main>
+    </div>
+  );
+}
