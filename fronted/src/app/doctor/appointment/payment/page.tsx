ec449b1581aa9043af6b1528c2b2f7e228af45e3
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-08-06 22:54:12 +0530

    updated link

--- fronted/src/app/doctor/appointment/payment/page.tsx
+++ fronted/src/app/doctor/appointment/payment/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/lin'
+import Link from 'next/link'
 
 export default function PaymentPage() {
   return (
