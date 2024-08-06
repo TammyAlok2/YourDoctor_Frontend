2be40fcb928c5f96360e4e25459d7d6630862bd1
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-08-06 22:56:30 +0530

    updated changes in payment

--- fronted/src/app/doctor/appointment/payment/page.tsx
+++ fronted/src/app/doctor/appointment/payment/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link'
 
+
 export default function PaymentPage() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center py-10">
