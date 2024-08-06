3950a60b7ac37183d4ea4b72ec1b2ad11dddfaf0
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-08-06 22:00:49 +0530

    making master branch

--- fronted/src/app/doctor/appointment/page.tsx
+++ fronted/src/app/doctor/appointment/page.tsx
@@ -2,6 +2,7 @@ import AppointmentSec1 from "@/components/Appointment sections/AppointmentSec1"
 import AppointmentSec2 from "@/components/Appointment sections/AppointmentSec2"
 import AppointmentForm from '@/components/Appointment sections/AppointmentForm'
 
+
 const Appointment = () =>{
   return (
     <div>
