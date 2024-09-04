b1ed8bdda9d7f885a6f7107e0760178198ed8e34
Pavan <129081053+Pavanpraja@users.noreply.github.com>
2024-09-04 18:36:41 +0530

    added footer page

--- fronted/src/app/layout.tsx
+++ fronted/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./GlobalRedux/provider";
 import Navbar from "@/components/Navbar";
-
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,10 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Providers>
-         <Navbar/>
-        {children}
-      </Providers>
+        <Providers>
+          <Navbar />
+          {children}
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
---
+++ fronted/src/components/Footer.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+
+const Footer = () => {
+    return (
+      <footer className="bg-gradient-to-b from-teal-500 to-teal-700 text-white py-10 px-[3rem]">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div>
+            <h3 className="font-semibold mb-4 text-[1.5rem]">Company</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li><Link href="#" className="text-[1.2rem] hover:underline">About Us</Link></li>
+              <li><Link href="#" className="text-[1.2rem] hover:underline">Careers</Link></li>
+              <li><Link href="#" className="text-[1.2rem] hover:underline">Blog</Link></li>
+              <li><Link href="#" className="text-[1.2rem] hover:underline">Partner with Yourlab</Link></li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="font-semibold mb-4 text-[1.5rem]">Support</h3>
+            <ul className="list-disc list-inside space-y-2">
+              <li className='list-block'><Link href="#" className="text-[1.2rem] hover:underline">Account</Link></li>
+              <li><Link href="#" className="text-[1.2rem] hover:underline">Legal</Link></li>
+              <li><Link href="#" className="text-[1.2rem] hover:underline">Contact</Link></li>
+              <li><Link href="#" className="text-[1.2rem] hover:underline">Terms & Conditions</Link></li>
+              <li><Link href="#" className="text-[1.2rem] hover:underline">Privacy Policy</Link></li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="font-semibold mb-4 text-[1.5rem]">Follow us on</h3>
+            {/* Add social media links here */}
+          </div>
+        </div>
+        <div className="mt-8 border-t border-gray-300 opacity-50"></div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
