a02907a1e991b83a2eea179afc0fac11593eec69
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-09-08 17:05:58 +0530

    added hoisted api

--- fronted/src/app/Helpers/axiosInstance.js
+++ fronted/src/app/Helpers/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-
-const BASE_URL =  "http://localhost:5000/api/v1/" //http://localhost:5000 , https://your-lab-userpage-backend.onrender.com
-
+const BASE_URL = 'https://api.yourlab.in/api/v1/' ;
+     //http://localhost:5000 , https://your-lab-userpage-backend.onrender.com
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
+
 axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
