dc46ca4c357f478347869837dd45bae350a9ec30
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-09-08 18:05:05 +0530

    solved some issue in login

--- fronted/src/app/login/page.js
+++ fronted/src/app/login/page.js
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 import { isEmail, isValidPassword } from "../Helpers/regexMatcher";
 import { toast } from "react-hot-toast";
 
-export default function Login({onBack, onBack1, setVisibleComponent, setSignupVisible}) {
+export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
   const [loginData, setLoginData] = useState({
     email: "",
@@ -16,10 +16,7 @@ export default function Login({onBack, onBack1, setVisibleComponent, setSignupVi
   const dispatch = useDispatch();
   const router = useRouter();
 
-  // const userData = useSelector((state) => state.data);
-  // console.log(userData);
-
-  function handleUserInput(e) {
+  function handleUserInput(e: React.ChangeEvent<HTMLInputElement>) {
     e.preventDefault();
     const { name, value } = e.target;
     setLoginData({
@@ -27,13 +24,12 @@ export default function Login({onBack, onBack1, setVisibleComponent, setSignupVi
       [name]: value,
     });
   }
-  // console.log(loginData);
 
-  const togglePasswordVisibility = (e) => {
+  const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  async function onLogin(event) {
+  async function onLogin(event: React.FormEvent) {
     event.preventDefault();
     if (!loginData.email || !loginData.password) {
       toast.error("Please fill all the details");
@@ -43,15 +39,14 @@ export default function Login({onBack, onBack1, setVisibleComponent, setSignupVi
       toast.error("Invalid email id");
       return;
     }
-    // checking password validation
     if (!isValidPassword(loginData.password)) {
       toast.error(
-        "Password should be 6 - 16 character long with atleast a number and special character"
+        "Password should be 6 - 16 characters long with at least a number and special character"
       );
       return;
     }
 
-    // dispatch create account action
+    // Dispatch login action
     const response = await dispatch(login(loginData));
     if (response?.payload?.success) {
       console.log(response);
@@ -60,8 +55,6 @@ export default function Login({onBack, onBack1, setVisibleComponent, setSignupVi
         email: "",
         password: "",
       });
-      setVisibleComponent(null)
-      setSignupVisible(false)
     }
   }
 
@@ -95,7 +88,7 @@ export default function Login({onBack, onBack1, setVisibleComponent, setSignupVi
             />
             <button
               type="button"
-              onClick={() => setShowPassword(!showPassword)}
+              onClick={togglePasswordVisibility}
               className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600 mt-3"
             >
               <img
@@ -107,7 +100,7 @@ export default function Login({onBack, onBack1, setVisibleComponent, setSignupVi
               />
             </button>
           </div>
-          <div className="flex justify-end mb-4 cursor-pointer" onClick={onBack1}>
+          <div className="flex justify-end mb-4 cursor-pointer">
             <a className="text-red-600 text-sm">
               Forgot Password?
             </a>
@@ -131,9 +124,6 @@ export default function Login({onBack, onBack1, setVisibleComponent, setSignupVi
             />
             Login with Google
           </button>
-          <div className="text-center cursor-pointer" onClick={onBack}>
-              {"Don’t"} have an account? Sign Up
-          </div>
         </form>
       </div>
     </div>
